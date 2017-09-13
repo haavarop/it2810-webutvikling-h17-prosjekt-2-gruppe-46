3f3b6ee0e5c9c00498f0570633424f83dc96fe62
Andreas Berger <Andreas.Berger.ab@gmail.com>
2017-09-13 15:57:15 +0200

    Create a basic node express server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/home', function (req, res) {
+  res.sendFile(__dirname+"/home.html")
+})
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+  res.send('404, four oh four', 404);
+});
+
+app.listen(8082, function () {
+  console.log('Example app listening on port 8082!')
+})

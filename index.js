852581eabe294e5b85237c41dee0d240eafd6393
haavarop <haavard@vaage.com>
2017-09-15 19:46:20 +0200

    Fix inconsistent use semicolon

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.get('/home', function (req, res) {
-  res.sendFile(__dirname+"/home.html")
-})
+  res.sendFile(__dirname+"/home.html");
+});
 
 //The 404 Route (ALWAYS Keep this as the last route)
 app.get('*', function(req, res){
@@ -17,4 +17,4 @@ app.get('*', function(req, res){
 
 app.listen(8082, function () {
   console.log('Example app listening on port 8082!')
-})
+});

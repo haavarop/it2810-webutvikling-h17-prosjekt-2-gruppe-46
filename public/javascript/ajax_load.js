5a0f07b92692674a9853c9a9bb75676ff287bea2
Erikkvo <Erikkvo@hotmail.com>
2017-09-22 13:18:05 +0200

    New logic behind loading html pages

--- public/javascript/ajax_load.js
+++ public/javascript/ajax_load.js
@@ -1,17 +1,29 @@
 
 console.log("Javascript has been found!");
 
+// $(document).ready(function(){
+//     $("#bortne").click(function(){
+//         console.log("Onclick is working!");
+//         //alert("hello");
+//         $("#change_content").load('/bortne.html') /*, function(responseTxt, statusTxt, xhr){
+//             if(statusTxt == "success")
+//                 alert("External content loaded successfully!");
+//             if(statusTxt == "error")
+//                 alert("Error: " + xhr.status + ": " + xhr.statusText);*/
+//     });
+// });
+
+
 $(document).ready(function(){
-    $("#bortne").click(function(){
+    $(".view").on('click', function(){
         console.log("Onclick is working!");
-        //alert("hello");
-        $("#change_content").load('/bortne.html') /*, function(responseTxt, statusTxt, xhr){
-            if(statusTxt == "success")
-                alert("External content loaded successfully!");
-            if(statusTxt == "error")
-                alert("Error: " + xhr.status + ": " + xhr.statusText);*/
+        var name = this.id;
+        console.log(name);
+        $("#change_content").load(name+'.html');
     });
 });
+
+
 
 /*$(document).ready(function(){
 $(".list li").slice(1).click(function(){

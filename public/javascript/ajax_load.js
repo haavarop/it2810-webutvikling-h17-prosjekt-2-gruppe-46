abc203cac5454d7304bf414905c37cad48380b67
bortne <kristsbo@stud.ntnu.no>
2017-09-24 14:12:48 +0200

    Remove music

--- public/javascript/ajax_load.js
+++ public/javascript/ajax_load.js
@@ -3,9 +3,6 @@ $(document).ready(function(){
     $(".main-content").load('landingpage.html');
     $(".view").on('click', function(){
         var name = this.id;
-        if (name != "bortne"){
-            stopMusic();
-        }
         $(".main-content").load(name+'.html');
 
     });

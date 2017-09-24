4b6d5fb3bc279962f8860f5fe8a419d21b63c363
Andreas Berger <Andreas.Berger.ab@gmail.com>
2017-09-24 13:51:11 +0200

    Niceify the landing page and add navbar functionality

--- public/javascript/ajax_load.js
+++ public/javascript/ajax_load.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+    $(".main-content").load('landingpage.html');
     $(".view").on('click', function(){
         var name = this.id;
         $(".main-content").load(name+'.html');
